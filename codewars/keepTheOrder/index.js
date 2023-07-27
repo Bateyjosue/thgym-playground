a2449e27a5239f7989517a4ebc441af886c1f27b
Josh Batey <josuebatey19@gmail.com>
2023-07-27 12:58:26 +0000

    Basic Code

---
+++ codewars/keepTheOrder/index.js
@@ -0,0 +1,11 @@
+function keepOrder(ary, val) {
+    let f = ary[0]
+    let l = ary[ary.length-1]
+    
+    if(ary.includes(val)){
+      
+    }
+    else {
+      if(val <= f) return 0
+    }
+}
