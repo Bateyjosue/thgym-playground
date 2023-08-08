159dc2d8b3dc5d320a2a42b7c19daae9cbeb4c69
Bateyjosue <josuebatey19@gmail.com>
2023-08-08 08:39:51 +0200

    Solve Counting Duplicates

--- codewars/countingDuplicates/index.js
+++ codewars/countingDuplicates/index.js
@@ -1,3 +1,12 @@
 function duplicateCount(text){
-    // your code here
+    let obj = {}
+    
+    text.toLowerCase().split('').forEach(el => {
+      if(Object.keys(obj).includes(el)){
+        obj[el]++
+      }else {
+        obj[el] = 1
+      }
+    })
+    return Object.entries(obj).filter(el => el[1] > 1).length
   }
