bf60f8ad4fdebdfbe43653bfd8120d6376353685
Bateyjosue <josuebatey19@gmail.com>
2023-08-09 20:19:09 +0200

    Add Question addLastName challenge

---
+++ ES6/Ex/addLastName.js
@@ -0,0 +1,12 @@
+
+function addLastName(...names){
+    names.length < 4 ?  console.log(`Not enough names`) : console.log(names)
+}
+
+
+
+addLastName('David') 
+addLastName('John', 'Doe') 
+addLastName('Jean', 'Marie', 'Vianney') 
+addLastName('Neymar', 'Silva', 'Santos', 'Junior') 
+addLastName('Lionel', 'Messi', 'Andreas', 'San', 'Cuccitini') 
