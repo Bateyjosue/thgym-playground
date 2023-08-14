d12178508be6491b35dc59a8cf215db503ea9b21
Bateyjosue <josuebatey19@gmail.com>
2023-08-14 11:44:02 +0200

    Add Footer and current location info

--- TS-Project/TS-PROECT/index.js
+++ TS-Project/TS-PROECT/index.js
@@ -1,31 +1,24 @@
+"use strict";
 // Add the properties to the Dashboard
 // This is a continuation of the Challenge, in which you are asked to add
 // the 3 properties image and title to the dashboard based on the 
 // properties array
-const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
-const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
-const userNameDisplay = document.querySelector('#user') as HTMLElement
-const footer = document.querySelector('footer') as HTMLElement
-
-function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
-    const iconDisplay = isLoyalty ? '⭐' : ''
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+const reviewTotalDisplay = document.querySelector('#reviews');
+const returningUserDisplay = document.querySelector('#returning-user');
+const userNameDisplay = document.querySelector('#user');
+const footer = document.querySelector('footer');
+function showReviewTotal(value, reviewer, isLoyalty) {
+    const iconDisplay = isLoyalty ? '⭐' : '';
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay;
 }
-
-function populateUser(isReturning : boolean, userName: string ) {
-    if (isReturning){
-        returningUserDisplay.innerHTML = 'back'
+function populateUser(isReturning, userName) {
+    if (isReturning) {
+        returningUserDisplay.innerHTML = 'back';
     }
-    userNameDisplay.innerHTML = userName
+    userNameDisplay.innerHTML = userName;
 }
-
 // Reviews
-const reviews : { 
-    name: string; 
-    stars: number; 
-    loyaltyUser: boolean; 
-    date: string
-    }[] = [
+const reviews = [
     {
         name: 'Sheia',
         stars: 5,
@@ -44,37 +37,17 @@ const reviews : {
         loyaltyUser: true,
         date: '27-03-2021'
     },
-]
-
+];
 // User
-const you: {
-    firstName: string;
-    lastName: string;
-    isReturning: boolean;
-    age: number;
-    stayedAt: string[]
-} = {
+const you = {
     firstName: 'Josh',
     lastName: 'Batey',
     isReturning: true,
     age: 35,
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
-}
-
+};
 // Array of Properties
-const properties : {
-    image: string;
-    title: string;
-    price: number;
-    location: {
-        firstLine: string;
-        city: string;
-        code: number;
-        country: string;
-    };
-    contact: string;
-    isAvailable: boolean;
-}[] = [
+const properties = [
     {
         image: './images/colombia-property.jpg',
         title: 'Colombian Shack',
@@ -86,7 +59,7 @@ const properties : {
             country: 'Colombia'
         },
         contact: 'marywinkle@gmail.com',
-        isAvailable: true  
+        isAvailable: true
     },
     {
         image: './images/poland-property.jpg',
@@ -99,7 +72,7 @@ const properties : {
             country: 'Poland'
         },
         contact: 'garydavis@hotmail.com',
-        isAvailable: false 
+        isAvailable: false
     },
     {
         image: './images/london-property.jpg',
@@ -114,30 +87,23 @@ const properties : {
         contact: 'andyluger@aol.com',
         isAvailable: true
     }
-]
-
+];
 // Functions
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
-
-populateUser(you.isReturning, you.firstName)
-
-
-function showProperties(properties:{image: string, title:string}[]){
-    const article = document.createElement('article')
-    const property = document.querySelector('.properties') as Element;
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+populateUser(you.isReturning, you.firstName);
+function showProperties(properties) {
+    const article = document.createElement('article');
+    const property = document.querySelector('.properties');
     properties.map(el => {
-        const {image, title } = el;
+        const { image, title } = el;
         property.innerHTML += `
         <article class="card">
             <h5>${title}</h5>
             <img src=${image} />
         </article>
-        `
+        `;
     });
-    
 }
-
-showProperties(properties)
-
-let currentLocation :[string, string, number] = ['Kigal-rw', '11:35AM', 26]
-footer.innerHTML = `${currentLocation[0]} | ${currentLocation[1]} | ${currentLocation[2]}`
+showProperties(properties);
+let currentLocation = ['Kigal-rw', '11:35AM', 26];
+footer.innerHTML = `${currentLocation[0]} | ${currentLocation[1]} | ${currentLocation[2]}`;
