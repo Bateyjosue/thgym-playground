515242b15d27c29ea8648ea3b572109658d8f722
Bateyjosue <josuebatey19@gmail.com>
2023-08-28 11:32:35 +0200

    FEAT: add triangle type challenge

---
+++ codewars/triangleType/index.ts
@@ -0,0 +1,15 @@
+/* Should return ᐃ type:
+  0 : if ᐃ cannot be made with given sides
+  1 : acute ᐃ
+  2 : right ᐃ
+  3 : obtuse ᐃ
+*/
+function radToDeg(rad :number) :number {
+    return rad * (180 / Math.PI)
+  }
+  
+  export function triangleType(a: number, b: number, c: number): number {
+    // your code here
+    return 0
+  }
+  
