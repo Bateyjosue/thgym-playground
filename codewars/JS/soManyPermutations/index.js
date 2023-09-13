b559a9f0c9d86a3d7902847c7443342926539164
Josh Batey <josuebatey19@gmail.com>
2023-09-13 09:07:04 +0000

    feat: solve So many permutations

--- codewars/JS/soManyPermutations/index.js
+++ codewars/JS/soManyPermutations/index.js
@@ -1,3 +1,10 @@
 function permutations(string) {
-	
+	if(string.length <= 2) return string.length === 2 ? [...new Set([string, string[1]+ string[0]])]  : [string]
+  return [... new Set(
+    string.split('')
+  .reduce(
+    (acc, curr, index) => 
+      acc.concat(permutations(string.slice(0, index) + string.slice(index + 1)).map(el => curr + el)),[]
+  )
+  )] 
 }
