114d27255f774610124369549c9bf9b074972cf2
Bateyjosue <josuebatey19@gmail.com>
2023-09-14 14:57:16 +0200

    feat: add the function to make my reduce to be call on an array as arrays method

--- Async/Callbacks/myReduce/myReduce.js
+++ Async/Callbacks/myReduce/myReduce.js
@@ -1,19 +1,37 @@
-function myReduce(arr, func, inialValue=null) {
+// function myReduce(arr, func, inialValue=null) {
+//     let value = 0;
+//     if(arr){
+//         for(let i = 0; i <arr.length; i++) {
+//             value = func(inialValue, arr[i])
+//             inialValue  = value
+//         }
+//         return value;
+//     }else return inialValue
+// }
+
+// const numbers = [1, 2,3,4]
+    
+// const sum = myReduce(numbers,(acc , curr) => {
+// 	return acc + curr 
+// }, 0)
+
+function myReduce(func, inialValue=null) {
     let value = 0;
-    if(arr){
-        for(let i = 0; i <arr.length; i++) {
-            value = func(inialValue, arr[i])
+    if(this){
+        for(let i = 0; i <this.length; i++) {
+            value = func(inialValue, this[i])
             inialValue  = value
         }
         return value;
     }else return inialValue
 }
 
-const numbers = [1, 2,3,4]
-    
-const sum = myReduce(numbers,(acc , curr) => {
-	return acc + curr 
+Array.prototype.myReduce = myReduce
+const numbers = [1, 2, 3, 4]
+
+const sum = numbers.myReduce((acc , curr) => {
+	return acc * curr 
 }, 0)
-    
-    console.log(sum)
+
+console.log(sum)
     // console.log(numbers.reduce((a, b) => a + b))
