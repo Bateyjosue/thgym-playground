6080a6677b662b78c70ac55ab05173b5b85e9f3f
Bateyjosue <josuebatey19@gmail.com>
2023-09-14 15:05:44 +0200

    feat: Add solution for the Sum of sum kata

--- codewars/JS/sumOfSum/index.js
+++ codewars/JS/sumOfSum/index.js
@@ -1,3 +1,12 @@
 function sumOfSums(n) {
-    return 0n
+    let sum = 0n
+    let innerSum = 0n 
+    for(let i = 1n; i <= n; i++){
+      innerSum += (i * (i + 1n)) / 2n
+    }
+    for(let j = 1n; j <= innerSum; j++){
+        sum += j
+      }
+    console.log(sum)
+    return sum;
 }
