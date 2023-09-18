524f78466c9143fd09cb9ea8280c31caeb20d8a0
Bateyjosue <josuebatey19@gmail.com>
2023-09-18 17:33:48 +0200

    add test for repeatEvery function

--- Async/Promises/index.js
+++ Async/Promises/index.js
@@ -3,3 +3,9 @@ function repeatEvery(func, time){
     timerID = setInterval(func, time)
 }
 
+console.log(repeatEvery(()=> {
+    if(timerID > 5000){
+        clearInterval(timerID)
+    }
+    console.log('Josh')
+},1000))
