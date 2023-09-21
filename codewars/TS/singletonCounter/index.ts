0e2bbf8f219aba035b4c634b156686bc21711b13
Bateyjosue <josuebatey19@gmail.com>
2023-09-21 15:38:08 +0200

    solve chalenge singleton counter

--- codewars/TS/singletonCounter/index.ts
+++ codewars/TS/singletonCounter/index.ts
@@ -1,4 +1,19 @@
 export class SingletonCounter {
-    // TODO:
+    counter: number
+    private static instance: SingletonCounter | null = null;
+    
+    private constructor(){
+      this.counter = 0;
+    }
+    
+    inc(){
+      ++this.counter
+      return this.counter
+    }
+    static getInstance():SingletonCounter {
+      if(!SingletonCounter.instance){
+        SingletonCounter.instance = new SingletonCounter()
+      }
+      return SingletonCounter.instance
+    }
   }
-  
