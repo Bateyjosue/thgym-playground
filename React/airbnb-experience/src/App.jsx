c92ca62bf781a3789a042e3d5a16f29b8280f720
Bateyjosue <josuebatey19@gmail.com>
2023-09-25 09:09:04 +0200

    feat: render Hero component : "

--- React/airbnb-experience/src/App.jsx
+++ React/airbnb-experience/src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 function Home() {
 
   return (
     <>
       <Header/>
+      <main className=''>
+        <Hero />
+      </main>
     </>
   )
 }
