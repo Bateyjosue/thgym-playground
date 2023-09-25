8318ba2e0b62df6a62355c1d1c977b55a372fdd6
Bateyjosue <josuebatey19@gmail.com>
2023-09-25 17:56:06 +0200

    feat: render client data form the data fetched

--- React/airbnb-experience/src/App.jsx
+++ React/airbnb-experience/src/App.jsx
@@ -1,15 +1,25 @@
 
 import './App.css'
+import Card from './components/Card'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import clients from '/src/data'
+
 
 function Home() {
 
   return (
     <>
       <Header/>
-      <main className=''>
+      <main className='overflow-hidden w-[100%]'>
         <Hero />
+        <section className='w-[120%] container mx-auto pl-8 mb-8 overflow-x-auto  lg:w-[70%]'>
+          <div className="clients scroll-0 overflow-x-hidden flex gap-4  ">
+            {clients.map((client, index) => (
+              <Card key={index} client={client} />
+            ))}
+          </div>
+        </section>
       </main>
     </>
   )
