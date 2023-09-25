ac2391d32765ee228c056e56e96fd65a8b9d4d49
Bateyjosue <josuebatey19@gmail.com>
2023-09-25 17:58:19 +0200

    fix style forht header to fill the page

--- React/airbnb-experience/src/components/Header.jsx
+++ React/airbnb-experience/src/components/Header.jsx
@@ -1,8 +1,7 @@
 import logo from '../assets/images/airbnb-logo.png';
-
 const Header = () => {
   return (
-    <header className='h-20 shadow-lg shadow-gray-500 flex container mx-auto px-8 py-6'>
+    <header className='h-[70px] max-w-[100%] shadow-lg shadow-gray-200 flex container mx-auto px-8 py-6'>
         <img src={logo} alt="" className='' />
     </header>
   )
