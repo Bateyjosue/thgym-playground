414efc14d36183438878570cffe8cd53dc5af178
Bateyjosue <josuebatey19@gmail.com>
2023-09-25 17:57:16 +0200

    feat: counsume client data to remove dummy data

--- React/airbnb-experience/src/components/Card.jsx
+++ React/airbnb-experience/src/components/Card.jsx
@@ -1,19 +1,27 @@
-import imgs from '../assets/images/katie-zaferes.png';
 import star from '../assets/images/star.png';
+import PropTypes from 'prop-types';
 
-const Card = () => {
+
+
+// eslint-disable-next-line react/prop-types
+const Card = ({client}) => {
+  // eslint-disable-next-line react/prop-types
+  const {id, title, location, coverImg, price, stats,openSpots} = client
+  // eslint-disable-next-line react/prop-types
+  const {rating, reviewCount} = stats
   return (
-    <article>
-      <span className='absolute m-2 px-2 bg-gray-100 rounded-sm text-[10px] font-normal'>SOLD OUT</span>
-      <img src={imgs} alt="" className='rounded-xl'/>
+    <article className='relative' id={id}>
+      {!openSpots && <span className='absolute m-2 px-2 bg-gray-100 rounded-sm text-[10px] font-normal'>SOLD OUT</span>}
+      
+      <img src={`/src/assets/images/${coverImg}`} alt="" className='rounded-xl'/>
       <div className='flex items-center my-2 gap-1 p-1 text-xs'>
         <img src={star} alt="" className='h-3.5 w-3.5' />
-        <h6 className='text-[#222]'>5.0</h6>
-        <p className='text-[#918E9B] font-light'>(9).USA</p>
+        <h6 className='text-[#222]'>{rating}</h6>
+        <p className='text-[#918E9B] font-light'>({reviewCount}).{location}</p>
       </div>
-      <div className='text-xs font-light'>Life lessons with Katie Zaferes</div>
+      <div className='text-xs font-light'>{title}</div>
       <div className='text-xs font-light flex gap-1'>
-        <span className='font-semibold'>From $136</span>
+        <span className='font-semibold'>From ${price}</span>
         <span>/</span>
         <span>person</span>
       </div>
@@ -22,3 +30,16 @@ const Card = () => {
 }
 
 export default Card
+
+Card.propsTypes = {
+  client: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    coverImg: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    stats: PropTypes.object.isRequired,
+    openSpots: PropTypes.number.isRequired,
+  })
+}
