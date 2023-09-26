d43eac550a382de7005dec7179228f234b4d30b6
Bateyjosue <josuebatey19@gmail.com>
2023-09-26 13:13:48 +0200

    render exercise dynamically

--- React/Exercise/src/App.tsx
+++ React/Exercise/src/App.tsx
@@ -1,28 +1,18 @@
-import {NavLink} from 'react-router-dom'
+import Exercise from './components/Exercise.jsx'
+import questions from './data/data'
+
 function Home() {
+  Exercise
 
   return (
     <>
       <main className="container mx-auto px-4 my-10">
         <section className="grid grid-cols-3 gap-2">
-          <article className="p-1 border rounded-xl">
-            <NavLink to='/hello-world'>
-              <h2 className="text-lg font-bold">Exercise 1: Hello World!</h2>
-            </NavLink>
-            <p className="text-light text-sm my-2">
-            Every programming language starter tutorial teaches you how to output "Hello, World!" using the language.
-            </p>
-            <ul className="list text-xs flex gap-1 ">
-              <li className="bg-gray-300 rounded-lg p-1 flex items-center underline">HTML</li>
-              <li className="bg-gray-300 rounded-lg p-1 flex items-center underline text-center ">
-                <a href="https://www.w3schools.com/css/css_align.asp" target="_black">
-                  How to center a text inside a div
-                </a>
-              </li>
-            </ul>
-          </article>
-          
-          
+          {
+            questions.map((el, i) => (
+              <Exercise key={i} {...el}/>
+            ))
+          }
         </section>
       </main>
       
