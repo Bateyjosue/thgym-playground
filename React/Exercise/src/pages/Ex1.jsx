b0d81085dae6ebcf07c0a3a718b266511cdda145
Bateyjosue <josuebatey19@gmail.com>
2023-09-26 09:08:20 +0200

    render header component to redirect to home

--- React/Exercise/src/pages/Ex1.jsx
+++ React/Exercise/src/pages/Ex1.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Header from '../components/Header'
 
 function HelloWorld(){
   return (
-    <main className='bg-yellow-400 max-h-full min-h-[100vh] flex items-center justify-center'>
-        <h1 className='text-2xl'>Hello, World!</h1>
-    </main>
+    <>
+        <Header />
+        <main className='bg-yellow-400 max-h-full min-h-[100vh] flex items-center justify-center'>
+            <h1 className='text-2xl'>Hello, World!</h1>
+        </main>
+    </>
   )
 }
 
