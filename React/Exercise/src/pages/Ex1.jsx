cec62fa1e8dedad1fff036731d6204593c7f6357
Bateyjosue <josuebatey19@gmail.com>
2023-09-26 08:56:11 +0200

    feat: add Hello World Component to render world message

---
+++ React/Exercise/src/pages/Ex1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HelloWorld(){
+  return (
+    <main className='bg-yellow-400 max-h-full min-h-[100vh] flex items-center justify-center'>
+        <h1 className='text-2xl'>Hello, World!</h1>
+    </main>
+  )
+}
+
+export default HelloWorld
