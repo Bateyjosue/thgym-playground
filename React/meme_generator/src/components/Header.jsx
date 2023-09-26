8c2d9d1dd0b6415ffad824dfd9c3a4bdb85a6d97
Bateyjosue <josuebatey19@gmail.com>
2023-09-26 16:36:17 +0200

    create Header Component

---
+++ React/meme_generator/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <img src="../assets/images/Troll.png" alt="" srcset="" />
+            <h2>Meme Generator</h2>
+        </div>
+        <nav>
+            <h3>React Course - Project 3</h3>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
