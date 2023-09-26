e07891ce8bc2d9565d947542c7d2e9b575608268
Bateyjosue <josuebatey19@gmail.com>
2023-09-26 15:05:42 +0200

    style exercise container

--- React/Exercise/src/App.tsx
+++ React/Exercise/src/App.tsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <>
       <main className="container mx-auto px-4 my-10">
-        <section className="grid grid-cols-3 gap-2">
+        <section className="grid grid-cols-2 lg:grid-cols-3 gap-2">
           {
             questions.map((el, i) => (
               <Exercise key={i} {...el}/>
