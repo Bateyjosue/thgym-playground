a0f6a1a508b7226126cfc4b2b6b2d68cedae9aba
Bateyjosue <josuebatey19@gmail.com>
2023-09-26 08:50:49 +0200

    add link to the hello world exercise Component

--- React/Exercise/src/App.tsx
+++ React/Exercise/src/App.tsx
@@ -1,11 +1,33 @@
-
-function App() {
+import {NavLink} from 'react-router-dom'
+function Home() {
 
   return (
     <>
-      <h1 className="text-5xl">Hello React Typescript</h1>
+      <main className="container mx-auto px-4 my-10">
+        <section className="grid grid-cols-3 gap-2">
+          <article className="p-1 border rounded-xl">
+            <NavLink to='/hello-world'>
+              <h2 className="text-lg font-bold">Exercise 1: Hello World!</h2>
+            </NavLink>
+            <p className="text-light text-sm my-2">
+            Every programming language starter tutorial teaches you how to output "Hello, World!" using the language.
+            </p>
+            <ul className="list text-xs flex gap-1 ">
+              <li className="bg-gray-300 rounded-lg p-1 flex items-center underline">HTML</li>
+              <li className="bg-gray-300 rounded-lg p-1 flex items-center underline text-center ">
+                <a href="https://www.w3schools.com/css/css_align.asp" target="_black">
+                  How to center a text inside a div
+                </a>
+              </li>
+            </ul>
+          </article>
+          
+          
+        </section>
+      </main>
+      
     </>
   )
 }
 
-export default App
+export default Home
--- React/Exercise/src/main.tsx
+++ React/Exercise/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import HelloWorld from './pages/Ex1.jsx'
+import Home from './App.tsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/hello-world' element={<HelloWorld />} />
+    </Routes>
+
+  </BrowserRouter>
+  ,
 )
