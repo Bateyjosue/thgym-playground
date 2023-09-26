92673435e2746868e2baaff037438be69834679e
Bateyjosue <josuebatey19@gmail.com>
2023-09-26 16:47:55 +0200

    add logo and navbar to header component

--- React/meme_generator/src/components/Header.jsx
+++ React/meme_generator/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import logo from '../assets/images/Troll.png'
 
 const Header = () => {
   return (
-    <header>
-        <div>
-            <img src="../assets/images/Troll.png" alt="" srcset="" />
-            <h2>Meme Generator</h2>
+    <header className='container mx-auto px-8 h-20 bg-gradient-to-r from-[#672280] to-[#A818DA] text-white flex justify-between items-center'>
+        <div className='flex items-center gap-2'>
+            <img src={logo} alt="" srcset="" className='w-8' />
+            <h2 className='text-xl font-bold'>Meme Generator</h2>
         </div>
-        <nav>
+        <nav className='text-sm'>
             <h3>React Course - Project 3</h3>
         </nav>
     </header>
