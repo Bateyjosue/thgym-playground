99a7164d4f97ed4c93665eff0c65cf8478e146a2
Bateyjosue <josuebatey19@gmail.com>
2023-10-01 14:56:58 +0200

    add link to exercise 3 custom component

--- React/Exercise/src/main.tsx
+++ React/Exercise/src/main.tsx
@@ -4,6 +4,7 @@ import UserClicks from './pages/Ex2'
 import Home from './App.tsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Custom_Component from './pages/Ex3.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Route path='/' element={<Home/>} />
       <Route path='/hello-world' element={<HelloWorld />} />
       <Route path='/user_clicks' element={<UserClicks />} />
+      <Route path='/custom_component' element={<Custom_Component />} />
     </Routes>
 
   </BrowserRouter>
