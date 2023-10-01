edfec1fb472c1c940bb368faa294fce98cb26d3a
Bateyjosue <josuebatey19@gmail.com>
2023-10-01 14:56:19 +0200

    render button component

--- React/Exercise/src/pages/Ex3.tsx
+++ React/Exercise/src/pages/Ex3.tsx
@@ -1,15 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import Button from '../components/Button'
 import Header from '../components/Header'
 
-const UserCLicks = () => {
+const Custom_Component = () => {
+    const handleClick = (e: Event) => {
+        e.preventDefault();
+        alert(`You have clicked on ${(e.target as HTMLInputElement).textContent}`)
+    }
   return (
     <>
         <Header />
-        <main className='container mx-auto px-8 pt-20'>
-            
+        <main className='container mx-auto px-8 pt-20  '>
+            <section className='mt-10 flex gap-4 justify-center'>
+                <Button name="Button 1 " handleClick={handleClick}/>
+                <Button name="Button 2" handleClick={handleClick}/>
+                <Button name="Button 3" handleClick={handleClick}/>
+            </section>
         </main>
     </>
   )
 }
 
-export default UserCLicks
+export default Custom_Component
