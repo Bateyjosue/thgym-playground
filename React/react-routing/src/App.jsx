5ef5eb290c7f0782b5d27dfa5c041f833dec8ae9
Bateyjosue <josuebatey19@gmail.com>
2023-10-03 18:07:04 +0200

    add render home page and config router

--- React/react-routing/src/App.jsx
+++ React/react-routing/src/App.jsx
@@ -1,12 +1,15 @@
-import Header from "./components/header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 
 function App() {
 
   return (
-    <>
-      <Header />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
