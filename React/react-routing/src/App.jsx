93da9b2f47e02e9a70c35051427e21801aecb6c2
Bateyjosue <josuebatey19@gmail.com>
2023-10-03 19:28:01 +0200

    import server data

--- React/react-routing/src/App.jsx
+++ React/react-routing/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Vans from "./pages/vans/Vans";
+import './data/server'
 
 
 function App() {
