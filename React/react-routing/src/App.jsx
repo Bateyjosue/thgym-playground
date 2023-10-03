14976e7c40bcc74b89cbb6c6de3d9f97c900fc79
Bateyjosue <josuebatey19@gmail.com>
2023-10-03 18:17:00 +0200

    render about page

--- React/react-routing/src/App.jsx
+++ React/react-routing/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import About from "./pages/about/About";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   )
--- React/react-routing/src/components/header/Header.jsx
+++ React/react-routing/src/components/header/Header.jsx
@@ -1,14 +1,24 @@
 // import React from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 
 const Header = () => {
   return (
     <header>
         <h1>
-            #VANLIFE
+            <Link to="/"> #VANLIFE</Link>
         </h1>
-        
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/vans">Vans</Link>
+                </li>
+            </ul>
+        </nav>
     </header>
   )
 }
