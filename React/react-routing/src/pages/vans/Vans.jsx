a91a703373e5d54b3b24f97d55a0df903a4ba9b5
Bateyjosue <josuebatey19@gmail.com>
2023-10-03 19:49:44 +0200

    style vans card

--- React/react-routing/src/pages/vans/Vans.jsx
+++ React/react-routing/src/pages/vans/Vans.jsx
@@ -24,8 +24,8 @@ const Vans = () => {
                     vans.map(van => (
                         <li key={van.id} className="van-tile">
                             <img src={van.imageUrl} alt="Van Avatar" />
-                            <div className="van-info">
-                                <h3>{van.name}</h3>
+                            <div className="van-info mb-2">
+                                <h3 className='font-bold'>{van.name}</h3>
                                 <p>${van.price}<span>/day</span></p>
                             </div>
                             <i className={`van-type ${van.type} selected`}>{van.type}</i>
