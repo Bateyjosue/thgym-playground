589e84cdf79e7d7d361560a22d90a8cbec950991
Bateyjosue <josuebatey19@gmail.com>
2023-10-03 18:16:27 +0200

    add layout about page

---
+++ React/react-routing/src/pages/about/About.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+import Header from '../../components/header/Header'
+
+const About = () => {
+  return (
+    <>
+        <Header />
+        <main>
+            <h1>About</h1>
+        </main>
+    </>
+  )
+}
+
+export default About
