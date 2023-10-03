4ecfa076d02e5af6d7d0e8f439116544147177bc
Bateyjosue <josuebatey19@gmail.com>
2023-10-03 19:28:39 +0200

    style header

--- React/react-routing/src/components/header/Header.jsx
+++ React/react-routing/src/components/header/Header.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header>
-        <h1>
+    <header className='h-20 bg-yellow-950 text-white container mx-auto px-8 flex justify-between items-center'>
+        <h1 className='text-2xl font-bold'>
             <Link to="/"> #VANLIFE</Link>
         </h1>
-        <nav>
-            <ul>
+        <nav className='font-medium'>
+            <ul className='flex gap-4'>
                 <li>
                     <Link to="/about">About</Link>
                 </li>
