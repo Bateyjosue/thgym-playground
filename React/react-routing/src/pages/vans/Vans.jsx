cae0e34770059bdbf101d69ff1054b7421b62e2c
Bateyjosue <josuebatey19@gmail.com>
2023-10-03 19:21:52 +0200

    fetch vans data from miragejs

--- React/react-routing/src/pages/vans/Vans.jsx
+++ React/react-routing/src/pages/vans/Vans.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import Header from '../../components/header/Header'
 
 const Vans = () => {
+    const [vans, setVans] = useState([])
+
+    useEffect(() =>{
+        fetch('/api/vans')
+        .then(res => res.json())
+        .then(data => setVans(data.vans))
+        .catch(err => console.log(`Network ERROR ${err.message}`))
+    }, [])
   return (
     <>
         <Header />
-        <main>
+        <main className='mx-20 my-12'>
             <h1>Vans page goes here 🚐</h1>
+            <ul className='list-disc'>
+                {
+                    vans.map(van => (
+                        <li key={van.id}>
+                            {/* <Link to={`/vans/${van.id}`}>{van.name}</Link> */}
+                            {van.name}
+                        </li>
+                    ))
+                }
+            </ul>
         </main>
     </>
   )
