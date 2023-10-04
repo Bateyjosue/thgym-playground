00289b4da3ddd552db439d7fb70861070e060d8a
Bateyjosue <josuebatey19@gmail.com>
2023-10-04 15:16:11 +0200

    add link to a single van

--- React/react-routing/src/pages/vans/Vans.jsx
+++ React/react-routing/src/pages/vans/Vans.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Header from '../../components/header/Header'
 import './van.css'
+import { Link } from 'react-router-dom'
 
 const Vans = () => {
     const [vans, setVans] = useState([])
@@ -21,12 +22,14 @@ const Vans = () => {
                     {
                         vans.map(van => (
                             <li key={van.id} className="van-tile">
-                                <img src={van.imageUrl} alt="Van Avatar" />
-                                <div className="van-info mb-2">
-                                    <h3 className='font-bold'>{van.name}</h3>
-                                    <p>${van.price}<span>/day</span></p>
-                                </div>
-                                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                                <Link to={van.id}>
+                                    <img src={van.imageUrl} alt="Van Avatar" />
+                                    <div className="van-info mb-2">
+                                        <h3 className='font-bold'>{van.name}</h3>
+                                        <p>${van.price}<span>/day</span></p>
+                                    </div>
+                                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                                </Link>
                             </li>
                         ))
                     }
