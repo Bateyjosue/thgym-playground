cda6256e2920c52d9f7206820e25e226ad4170eb
Bateyjosue <josuebatey19@gmail.com>
2023-10-04 15:13:52 +0200

    add Vandetails pages to diplay single van

---
+++ React/react-routing/src/pages/vans/VanDetails.jsx
@@ -0,0 +1,36 @@
+import {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom'
+import Header from '../../components/header/Header'
+
+const VanDetails = () => {
+  const [vanDetails, setVanDetails] = useState({})
+    const {id} = useParams()
+    useEffect(()=> {
+      fetch(`/api/vans/${id}`)
+      .then(res => res.json())
+      .then(data => setVanDetails(data.vans))
+    }, [id])
+
+    const {name, description, imageUrl, price, type} = vanDetails
+  return (
+    <>
+        <Header/>
+        <main className='container mx-auto'>
+          <div className="van-detail-container">
+              {vanDetails ? (
+                  <div className="van-detail">
+                      <img src={imageUrl} alt=""/>
+                      <i className={`van-type ${type} selected`}>{type}</i>
+                      <h2>{name}</h2>
+                      <p className="van-price"><span>${price}</span>/day</p>
+                      <p>{description}</p>
+                      <button className="link-button">Rent this van</button>
+                  </div>
+              ) : <h2>Loading...</h2>}
+          </div>
+        </main>
+    </>
+  )
+}
+
+export default VanDetails
