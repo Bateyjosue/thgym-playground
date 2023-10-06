e066cefad8584ddcc93628e475ee85f054d75b96
Bateyjosue <josuebatey19@gmail.com>
2023-10-06 08:18:23 +0200

    style navigation bar active link

--- React/react-routing/src/components/header/Header.jsx
+++ React/react-routing/src/components/header/Header.jsx
@@ -1,24 +1,37 @@
 // import React from 'react'
 import './Header.css'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
+const navStyles = {
+    fontWeight: 'bold',
+    textDecoration: 'underline #ff3028 wavy',
+    color: '#fff',
+    textUnderlineOffset: '5px'
+}
 
 const Header = () => {
   return (
     <header className='h-20 bg-yellow-950 text-white container mx-auto px-8 flex justify-between items-center min-w-full'>
         <h1 className='text-2xl font-bold'>
-            <Link to="/"> #VANLIFE</Link>
+            <NavLink
+                to="/"> #VANLIFE</NavLink>
         </h1>
         <nav className='font-medium'>
             <ul className='flex gap-4'>
                 <li>
-                    <Link to="/host">Host</Link>
+                    <NavLink
+                        style={({isActive})=> isActive ? navStyles : null}
+                        to="/host">Host</NavLink>
                 </li>
                 <li>
-                    <Link to="/about">About</Link>
+                    <NavLink
+                        style={({isActive})=> isActive ? navStyles : null}
+                        to="/about">About</NavLink>
                 </li>
                 <li>
-                    <Link to="/vans">Vans</Link>
+                    <NavLink
+                        style={({isActive})=> isActive ? navStyles : null}
+                        to="/vans">Vans</NavLink>
                 </li>
             </ul>
         </nav>
