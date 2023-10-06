8605832aa26a6767fdd6dd23fcf1c40ce337c9a2
Bateyjosue <josuebatey19@gmail.com>
2023-10-06 08:33:49 +0200

    style active link for the host layout

--- React/react-routing/src/components/layout/HostLayout.jsx
+++ React/react-routing/src/components/layout/HostLayout.jsx
@@ -1,13 +1,26 @@
-import { Link, Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 import './layout.css'
+
+const navStyles = {
+  background: 'rgb(66 32 6)',
+  opacity: '.2',
+  color: 'white',
+}
 
 const HostLayout = () => {
   return (
     <>
-        <nav className="host-nav">
-            <Link to="/host">Dashboard</Link>
-            <Link to="/host/income">Income</Link>
-            <Link to="/host/reviews">Reviews</Link>
+        <nav className="host-nav my">
+            <NavLink 
+              end
+              style={({isActive})=> isActive ? navStyles : null}
+              to="/host">Dashboard</NavLink>
+            <NavLink 
+              style={({isActive})=> isActive ? navStyles : null}
+              to="/host/income">Income</NavLink>
+            <NavLink 
+              style={({isActive})=> isActive ? navStyles : null}
+              to="/host/reviews">Reviews</NavLink>
         </nav>
         <Outlet />
     </>
