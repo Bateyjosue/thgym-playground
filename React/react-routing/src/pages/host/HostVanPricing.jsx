a2b33b6ace86ecaa915e29335491e872fdbd93be
Bateyjosue <josuebatey19@gmail.com>
2023-10-09 10:48:41 +0200

    add host van pricing component

---
+++ React/react-routing/src/pages/host/HostVanPricing.jsx
@@ -0,0 +1,17 @@
+import { useOutletContext } from "react-router-dom"
+
+const HostVanPricing = () => {
+    const [van] = useOutletContext()
+  const {price} = van
+
+  if (!van) {
+    return <h1>Loading...</h1>
+}
+  return (
+    <div>
+        <p><span className="font-bold">${price}</span>/day</p>
+    </div>
+  )
+}
+
+export default HostVanPricing
