017204f4cabf02d99c01b095fff28909a1cdf2ab
Bateyjosue <josuebatey19@gmail.com>
2023-10-09 10:55:00 +0200

    update vans link

--- React/react-routing/src/pages/host/HostVan.jsx
+++ React/react-routing/src/pages/host/HostVan.jsx
@@ -1,15 +1,86 @@
 // import React from 'react'
-import { NavLink, useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { NavLink, Link, Outlet, useParams } from "react-router-dom"
+import {getHostVan} from "../../data/fetchData"
+
+const active = {
+  textDecoration: 'underline',
+}
 
 const HostVan = () => {
-  return (
-    <div>
-      <NavLink to='/host/vans' className='underline text-blue-500'>Back to vans</NavLink>
-      <p>
+  const [van, setVan] = useState([])
+  const {id} = useParams()
 
-      Host single Van {useParams().id}
-      </p>
-    </div>
+
+  useEffect(() => {
+    try{
+      (
+        async() => {
+          const van = await getHostVan(id)
+          setVan(van.vans)
+        }
+      )()
+    }
+    catch(err){
+      console.log(err);
+    }
+  }, [id])
+  const {name, imageUrl, type, price} = van
+
+  if (!van) {
+    return <h1>Loading...</h1>
+}
+  return (
+    <section className="container mx-auto">
+      <Link
+        to=".."
+        relative="path"
+        className="back-button"
+        >&larr; <span>Back to all vans</span></Link>
+      <div className="host-van-detail-layout-container">
+          <div className="host-van-detail">
+              <img src={imageUrl} alt="" />
+              <div className="host-van-detail-info-text">
+                  <i
+                      className={`van-type van-type-${type}`}
+                  >
+                      {type}
+                  </i>
+                  <h3>{name}</h3>
+                  <h4>${price}/day</h4>
+              </div>
+          </div>
+      </div>
+      <div className="mini-nav flex gap-4 my-4">
+        <NavLink
+        end 
+        to={`/host/vans/${id}`}
+        style={({isActive}) => { isActive? active : null}}
+        >
+          <div className="mini-nav-item">
+            <i className="fas fa-info-circle"></i>
+            <span>Info</span>
+          </div>
+        </NavLink>
+        <NavLink to={`/host/vans/${id}/pricing`}
+          style={({isActive}) => { isActive? active : null}}
+        >
+          <div className="mini-nav-item">
+            <i className="fas fa-dollar-sign"></i>
+            <span>Pricing</span>
+          </div>
+        </NavLink>
+        <NavLink to={`/host/vans/${id}/photos`}
+          style={({isActive}) => { isActive? active : null}}
+        >
+          <div className="mini-nav-item">
+            <i className="fas fa-images"></i>
+            <span>Photos</span>
+          </div>
+        </NavLink>
+      </div>
+      <Outlet context={[van]}/>
+    </section>
   )
 }
 
