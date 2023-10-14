1ab45477c3d2d5c7c57cfa4daa5ef8ab0ec98613
Bateyjosue <josuebatey19@gmail.com>
2023-10-14 23:38:16 +0200

    refactor: change the logic to not render the buttons for vans category if vans dont exist

--- React/react-routing/src/pages/vans/Vans.jsx
+++ React/react-routing/src/pages/vans/Vans.jsx
@@ -19,65 +19,68 @@ const Vans = () => {
 
   return (
     <>
-      <main className=' my-12'>
-        <h1 className='text-4xl text-center font-bold'>Vans page goes here 🚐</h1>
-        <div className='van-list-filter-buttons'>
-          <button 
-            className={`van-type simple ${typeFilter === 'simple' ? 'selected' : ''}`}
-            onClick={()=> setSearchParams({type: 'simple'})}
-            >Simple</button>
-          <button 
-            className={`van-type rugged ${typeFilter === 'rugged' ? 'selected' : ''}`}
-            onClick={()=> setSearchParams({type: 'rugged'})}>Rugged</button>
-          <button 
-            className={`van-type luxury ${typeFilter === 'luxury' ? 'selected' : ''}`}
-            onClick={()=> setSearchParams({type: 'luxury'})}>Luxury</button>
-            {
-              typeFilter ? (
-                <button 
-                    className='van-type clear-filters'
-                    onClick={()=> setSearchParams({})}>
-                    Clear All
-                  </button>
-              ) : null
-            }
-          
-        </div>
-        <section className='van-list-container'>
-          <ul className='van-list'>
-            <Suspense fallback={<h1 className='text-5xl font-bold'>Loading...</h1>}>
-              <Await resolve={vans.vans}>
+      <Suspense 
+        fallback={<h1 className='text-5xl font-bold'>Loading...</h1>}
+        errorElement={<h1 className='text-5xl font-bold'>Error on fetching...</h1>}
+      >
+          <main className=' my-12'>
+            <h1 className='text-4xl text-center font-bold'>Explore Our Vans 🚐</h1>
+            <div className='van-list-filter-buttons'>
+              <button 
+                className={`van-type simple ${typeFilter === 'simple' ? 'selected' : ''}`}
+                onClick={()=> setSearchParams({type: 'simple'})}
+                >Simple</button>
+              <button 
+                className={`van-type rugged ${typeFilter === 'rugged' ? 'selected' : ''}`}
+                onClick={()=> setSearchParams({type: 'rugged'})}>Rugged</button>
+              <button 
+                className={`van-type luxury ${typeFilter === 'luxury' ? 'selected' : ''}`}
+                onClick={()=> setSearchParams({type: 'luxury'})}>Luxury</button>
                 {
-                  (loadVans)=> {
-                    const filterVans = typeFilter 
-                      ? loadVans.filter(van => van.type === typeFilter) 
-                      : loadVans
-                    return (
-                      filterVans.map(van => (
-                        <li key={van.id} className="van-tile">
-                          <Link to={van.id}
-                            state={{
-                              search: searchParams.toString(),
-                              type: typeFilter
-                            }}
-                            >
-                            <img src={van.imageUrl} alt="Van Avatar" />
-                            <div className="van-info mb-2">
-                              <h3 className='font-bold'>{van.name}</h3>
-                              <p>${van.price}<span>/day</span></p>
-                            </div>
-                            <i className={`van-type ${van.type} selected`}>{van.type}</i>
-                          </Link>
-                        </li>
-                      ))
-                    )
-                  }
+                  typeFilter ? (
+                    <button 
+                        className='van-type clear-filters'
+                        onClick={()=> setSearchParams({})}>
+                        Clear All
+                      </button>
+                  ) : null
                 }
-              </Await>
-            </Suspense>
-          </ul>
-        </section>
-      </main>
+              
+            </div>
+            <section className='van-list-container'>
+              <ul className='van-list'>
+                  <Await resolve={vans.vans}>
+                    {
+                      (loadVans)=> {
+                        const filterVans = typeFilter 
+                          ? loadVans.filter(van => van.type === typeFilter) 
+                          : loadVans
+                        return (
+                          filterVans.map(van => (
+                            <li key={van.id} className="van-tile">
+                              <Link to={van.id}
+                                state={{
+                                  search: searchParams.toString(),
+                                  type: typeFilter
+                                }}
+                                >
+                                <img src={van.imageUrl} alt="Van Avatar" />
+                                <div className="van-info mb-2">
+                                  <h3 className='font-bold'>{van.name}</h3>
+                                  <p>${van.price}<span>/day</span></p>
+                                </div>
+                                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                              </Link>
+                            </li>
+                          ))
+                        )
+                      }
+                    }
+                  </Await>
+              </ul>
+            </section>
+          </main>
+    </Suspense>
     </>
   )
 }
