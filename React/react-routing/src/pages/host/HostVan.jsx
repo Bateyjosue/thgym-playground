5ce76b5eb5eb0fb8de90df2fc9bd6d420d023a2e
Bateyjosue <josuebatey19@gmail.com>
2023-10-15 00:47:13 +0200

    refactor: update hostVanDetail component to use deferred data API

--- React/react-routing/src/pages/host/HostVan.jsx
+++ React/react-routing/src/pages/host/HostVan.jsx
@@ -1,13 +1,14 @@
-import { NavLink, Link, Outlet, useLoaderData } from "react-router-dom"
+import { NavLink, Link, Outlet, useLoaderData, defer, Await } from "react-router-dom"
 import {getHostVans} from "../../data/fetchData"
 import { requireAuth } from "../../data/utils"
+import { Suspense } from "react"
 
 export async function loader({params, request}){
-  // const auth = await requireAuth(request)
-  //   if(auth != null) {
-    // }
+  const hostVanDetail = getHostVans(params.id)
   requireAuth(request)
-  return getHostVans(params.id)
+  return defer({
+    hostVanDetail: hostVanDetail
+  })
 }
 
 const activeStyles = {
@@ -15,13 +16,8 @@ const activeStyles = {
 }
 
 const HostVan = () => {
-  const van = useLoaderData() || []
+  const van = useLoaderData()
 
-  const {name, imageUrl, type, price} = van
-
-  if (!van) {
-    return <h1>Loading...</h1>
-}
   return (
     <section className="container mx-auto ">
       <Link
@@ -29,39 +25,50 @@ const HostVan = () => {
         relative="path"
         className="back-button"
         >&larr; <span>Back to all vans</span></Link>
-      <div className="host-van-detail-layout-container">
-          <div className="host-van-detail">
-              <img src={imageUrl} alt="" />
-              <div className="host-van-detail-info-text">
-                  <i
-                      className={`van-type van-type-${type}`}
-                  >
-                      {type}
-                  </i>
-                  <h3>{name}</h3>
-                  <h4>${price}/day</h4>
-              </div>
-          </div>
-        <nav className="host-van-detail-nav">
-          <NavLink
-            to="."
-            end
-            style={({ isActive }) => isActive ? activeStyles : null}>
-              Details
-          </NavLink>
-          <NavLink
-            to="pricing"
-            style={({ isActive }) => isActive ? activeStyles : null}>
-              Pricing
-          </NavLink>
-          <NavLink
-            to="photos"
-            style={({ isActive }) => isActive ? activeStyles : null}>
-              Photos
-          </NavLink>
-        </nav>
-        <Outlet context={[van]}/>
-      </div>
+          <Suspense fallback={<h1 className="text-5xl font-bold">Loading...</h1>}>
+            <Await resolve={van.hostVanDetail}>
+              {
+                (van) => {
+                  const {name, imageUrl, type, price} =van
+                  return ( 
+                <div className="host-van-detail-layout-container">
+                    <div className="host-van-detail">
+                      <img src={imageUrl} alt="" />
+                      <div className="host-van-detail-info-text">
+                          <i
+                              className={`van-type van-type-${type}`}
+                          >
+                              {type}
+                          </i>
+                          <h3>{name}</h3>
+                          <h4>${price}/day</h4>
+                      </div>
+                    </div>
+                    <nav className="host-van-detail-nav">
+                      <NavLink
+                        to="."
+                        end
+                        style={({ isActive }) => isActive ? activeStyles : null}>
+                          Details
+                      </NavLink>
+                      <NavLink
+                        to="pricing"
+                        style={({ isActive }) => isActive ? activeStyles : null}>
+                          Pricing
+                      </NavLink>
+                      <NavLink
+                        to="photos"
+                        style={({ isActive }) => isActive ? activeStyles : null}>
+                          Photos
+                      </NavLink>
+                    </nav>
+                    <Outlet context={[van]}/>
+                  </div>
+                  )
+                }
+              }
+            </Await>
+          </Suspense>
     </section>
   )
 }
