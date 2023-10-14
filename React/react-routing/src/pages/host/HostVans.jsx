eb50dd092737629b5ddc0661e11d17ac1860caf7
Bateyjosue <josuebatey19@gmail.com>
2023-10-15 00:32:27 +0200

    refactor: update hostVans COmponent to use deferred API to fetch data

--- React/react-routing/src/pages/host/HostVans.jsx
+++ React/react-routing/src/pages/host/HostVans.jsx
@@ -1,42 +1,49 @@
-import { NavLink, useLoaderData } from "react-router-dom"
+import { Await, NavLink, defer, useLoaderData } from "react-router-dom"
 import './host.css'
 import { getHostVans } from '../../data/fetchData'
 import { requireAuth } from "../../data/utils"
+import { Suspense } from "react"
 
-export async function loader({request}){
-    // const auth = await requireAuth(request)
-    // if(auth != null) {
-    //     return requireAuth(request)
-    // }
+export  function loader({request}){
+    const hostVanData =  getHostVans()
     requireAuth(request)
-    return getHostVans()
+    return defer({
+        hostVans: hostVanData
+    })
 }
 
 const HostVans = () => {
-  const vans = useLoaderData() || []
-
-    const hostVansEls = vans.map(van => (
-        <NavLink
-            to={van.id}
-            key={van.id}
-            className="host-van-link-wrapper"
-        >
-            <div className="host-van-single" key={van.id}>
-                <img src={van.imageUrl} alt="vans avatar" />
-                <div className="host-van-info">
-                    <h3>{van.name}</h3>
-                    <p>${van.price}/day</p>
-                </div>
-            </div>
-        </NavLink>
-    ))
+  const vans = useLoaderData()
 
     return (
         <section>
-            <h1 className="host-vans-title">Your listed vans</h1>
+            <h1 className="host-vans-title text-5xl font-bold my-4">Your listed vans</h1>
             <div className="host-vans-list">
                 <section>
-                    {hostVansEls}
+                    <Suspense fallback={<h1 className="text-5xl font-bold">Loading...</h1>}>
+                        <Await resolve={vans.hostVans}>
+                            { (hostVans)=>{
+                                    return (
+                                        hostVans.map(van => (
+                                            <NavLink
+                                                to={van.id}
+                                                key={van.id}
+                                                className="host-van-link-wrapper"
+                                            >
+                                                <div className="host-van-single" key={van.id}>
+                                                    <img src={van.imageUrl} alt="vans avatar" />
+                                                    <div className="host-van-info">
+                                                        <h3>{van.name}</h3>
+                                                        <p>${van.price}/day</p>
+                                                    </div>
+                                                </div>
+                                            </NavLink>
+                                        ))
+                                    )
+                                }
+                            }
+                        </Await>
+                    </Suspense>
                 </section>
             </div>
         </section>
