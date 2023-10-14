823dd5d772db5d2dff30d9d8ecc3509537c6206d
Bateyjosue <josuebatey19@gmail.com>
2023-10-15 00:05:48 +0200

    refactor: van Component to use defer

--- React/react-routing/src/pages/vans/VanDetails.jsx
+++ React/react-routing/src/pages/vans/VanDetails.jsx
@@ -1,17 +1,19 @@
-import {Link, useLoaderData, useLocation} from 'react-router-dom'
+import {Await, Link, defer, useLoaderData, useLocation} from 'react-router-dom'
 import {getVans} from '../../data/fetchData'
+import { Suspense } from 'react'
 
 export async function loader({params}){
-  return await getVans(params.id)
+  const vanData = getVans(params.id)
+  return defer({
+    van: vanData,
+  })
 }
 
 const VanDetails = () => {
   const location = useLocation()
   const typeFilter = location.state?.type || 'all'
-
   const vanDetails = useLoaderData()
 
-    const {name, description, imageUrl, price, type} = vanDetails
   return (
     <>
         <main className='container mx-auto'>
@@ -22,16 +24,24 @@ const VanDetails = () => {
               className='back-button hover:underline'>
                 &larr; Back to {typeFilter} vans
             </Link>
-              {vanDetails ? (
-                  <div className="van-detail">
-                      <img src={imageUrl} alt=""/>
-                      <i className={`van-type ${type} selected`}>{type}</i>
-                      <h2>{name}</h2>
-                      <p className="van-price"><span>${price}</span>/day</p>
-                      <p>{description}</p>
-                      <button className="link-button">Rent this van</button>
-                  </div>
-              ) : <h2>Loading...</h2>}
+              <Suspense fallback={<h1 className='text-5xl font-bold'>Fetching...</h1>}>
+                <Await resolve={vanDetails.van}>
+                  {(vanDetail) => {
+                      const {name, description, imageUrl, price, type} = vanDetail
+                        return (
+                          <div className="van-detail">
+                            <img src={imageUrl} alt=""/>
+                            <i className={`van-type ${type} selected`}>{type}</i>
+                            <h2>{name}</h2>
+                            <p className="van-price"><span>${price}</span>/day</p>
+                            <p>{description}</p>
+                            <button className="link-button">Rent this van</button>
+                        </div>
+                        )
+                    }
+                  }
+                </Await> 
+            </Suspense>
           </div>
         </main>
     </>
